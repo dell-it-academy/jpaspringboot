ab1c3b05aeef4fde007cccc34f258ff426205c78
coliveira <Cleysonb.oliveira@gmail.com>
2019-08-13 15:35:35 -0300

    spring starter

---
+++ src/test/java/jpa/spring/jpaspringboot/JpaspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package jpa.spring.jpaspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaspringbootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
